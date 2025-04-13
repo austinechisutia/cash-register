d4755a1feaaab1ff17a8ac0fd0281fe5ffe0906e
austinechisutia <austineakhonya624@gmail.com>
2025-04-13 15:15:07 +0300

    React1

---
+++ cashregister/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <header className="header">
+            <h1>Cash Register</h1>
+            <h2>By: Your Name</h2>
+            <h3>Version: 1.0</h3>
+
+            </header>
+    )
+}
+export default Header
---
+++ myReact project/src/food.jsx
@@ -0,0 +1,21 @@
+function Food() {
+    const food1 = "Pizza";
+    const food2 = "Burger";
+    const food3 = "Salad";
+    const food4 = "Sushi";
+    const food5 = "Pasta";
+    const food6 = "Rice";
+    const food7 = "Chicken";
+    return (
+      <ul>
+        <li>{food1}</li>
+        <li>{food2}</li>
+        <li>{food3}</li>
+        <li>{food4}</li>
+        <li>{food5}</li>
+        <li>{food6}</li>
+        <li>{food7}</li>
+      </ul>
+    )
+}
+export default Food;
